08c54db606343e6fe8518c5ae981aacfe7c8f46d
hgurnel <hgurnel@gmail.com>
2020-10-23 16:15:53 +0200

    Simplify vertex-shader source code using raw string literals

--- learn_opengl/src/main.cpp
+++ learn_opengl/src/main.cpp
@@ -9,12 +9,18 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
 // Shader source codes
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
+
+// Using raw string literals to avoid writing \n constantly
+const char* vertexShaderSource = R"glsl(
+#version 330 core
+
+layout(location = 0) in vec3 aPos;
+
+void main()
+{
+   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+}
+)glsl";
 
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
