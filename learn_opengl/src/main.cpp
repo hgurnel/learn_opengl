b78d07a212cb81826cd5cf7c34a7fdd1d2834bf8
hgurnel <hgurnel@gmail.com>
2020-10-23 16:36:05 +0200

    Add toggle for wireframe view

--- learn_opengl/src/main.cpp
+++ learn_opengl/src/main.cpp
@@ -194,6 +194,10 @@ void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+    if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS)
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS)
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 }
 
 // Callback fct that is called every time the window is resized
