915108722c5d1bb2eb77629d614f7b84d4149ba5
hadrien gurnel <hadrien.gurnel@epfl.ch>
2020-10-27 17:37:08 +0100

    Generate texture object, load an image and attach image to texture

--- learn_opengl/src/main.cpp
+++ learn_opengl/src/main.cpp
@@ -3,6 +3,7 @@
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 #include "../header/Shader.h"
+#include "../header/stb_image.h"
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -43,6 +44,39 @@ int main()
         std::cout << "Failed to initialise GLAD" << std::endl;
         return -1;
     }
+
+    // TEXTURE
+
+    // Generate a texture object
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    // Bind texture
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    // set the texture wrapping/filtering options (on the currently bound texture object)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // Load image
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load("../textures/container.jpg", &width, &height, &nrChannels, 0);
+    
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    
+    // Free image memory
+    stbi_image_free(data);
+
+
 
     // SHADERS   
     Shader ourShader("shader.vs", "shader.fs");
