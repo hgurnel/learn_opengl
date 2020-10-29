e9f4db2e66e437858791dfec40644de3eeb1c59b
hadrien gurnel <hadrien.gurnel@epfl.ch>
2020-10-29 16:09:45 +0100

    Make only the smiley look in the reverse direction

--- learn_opengl/shader.fs
+++ learn_opengl/shader.fs
@@ -10,5 +10,5 @@ uniform sampler2D fsTexture2;
 void main()
 {
     // Mix of 0.2: 80% texture1 / 20% texture2
-    FragColor = mix(texture(fsTexture1, TexCoord), texture(fsTexture2, TexCoord), 0.2);
+    FragColor = mix(texture(fsTexture1, TexCoord), texture(fsTexture2, TexCoord*vec2(-1.0, 1.0)), 0.2);
 }
